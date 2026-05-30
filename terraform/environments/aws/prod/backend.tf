cf6e98320eb3a3edf9644712ed9e89bdee77f135
Fabian Vargas <fabian_evm@live.com>
2026-05-30 18:42:39 -0500

    refactorizacion 128

--- terraform/environments/aws/prod/backend.tf
+++ terraform/environments/aws/prod/backend.tf
@@ -1,14 +1,9 @@
 terraform {
-
   backend "s3" {
-
     bucket         = "terraform-state-fabian-multicloud"
     key            = "aws-prod.tfstate"
     region         = "us-east-1"
-
     dynamodb_table = "terraform-locks"
-
-    encrypt = true
-
+    encrypt        = true
   }
 }
