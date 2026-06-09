a0ac18e2d1d3e6a400f4d7f642e7d1a71cfe729d
Fabian Vargas <fabian_evm@live.com>
2026-06-08 23:21:30 -0500

    fix pipeline app-multicloud 167

--- terraform/providers/aws/compute/main.tf
+++ terraform/providers/aws/compute/main.tf
@@ -1,19 +1,19 @@
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
 resource "aws_key_pair" "key" {
   key_name   = "${var.name_prefix}-key"
-  public_key = var.ssh_public_key
+  public_key = tls_private_key.ssh.public_key_openssh
 }
 
 resource "aws_instance" "vm" {
-  ami = data.aws_ami.ubuntu.id
-  instance_type          = var.instance_type
-
-  subnet_id              = var.subnet_id
-
-  vpc_security_group_ids = [
-    var.security_group_id
-  ]
-
-  iam_instance_profile = var.instance_profile_name
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  subnet_id                  = var.subnet_id
+  vpc_security_group_ids     = [var.security_group_id]
+  iam_instance_profile       = var.instance_profile_name
 
   key_name = aws_key_pair.key.key_name
 
@@ -27,24 +27,17 @@ resource "aws_instance" "vm" {
   })
 
   root_block_device {
-
-  volume_size = 30
-
-  volume_type = "gp3"
-
-  encrypted   = true
-}
+    volume_size = 30
+    volume_type = "gp3"
+    encrypted   = true
+  }
 }
 
 resource "aws_eip" "vm_ip" {
-
   instance = aws_instance.vm.id
+  domain   = "vpc"
 
-  domain = "vpc"
-
-  depends_on = [
-    aws_instance.vm
-  ]
+  depends_on = [aws_instance.vm]
 
   tags = merge(var.tags, {
     Name = "${var.name_prefix}-eip"
--- terraform/providers/aws/compute/outputs.tf
+++ terraform/providers/aws/compute/outputs.tf
@@ -5,3 +5,8 @@ output "public_ip" {
 output "instance_id" {
   value = aws_instance.vm.id
 }
+
+output "ssh_private_key" {
+  value     = tls_private_key.ssh.private_key_pem
+  sensitive = true
+}
