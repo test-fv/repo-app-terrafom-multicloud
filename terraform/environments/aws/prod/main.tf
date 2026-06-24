abb612115aa7f70d4759be153092d20be7203b64
Fabian Vargas <fabian_evm@live.com>
2026-06-23 21:58:48 -0500

    fix: terraform fmt

--- terraform/environments/aws/prod/main.tf
+++ terraform/environments/aws/prod/main.tf
@@ -2,17 +2,17 @@ module "platform" {
 
   source = "../../../stacks/aws/app-platform"
 
-  name_prefix     = local.name_prefix
+  name_prefix = local.name_prefix
 
-  instance_type   = local.instance_type
+  instance_type = local.instance_type
 
   repository_name = local.repository_name
 
-  aws_region      = local.aws_region
+  aws_region = local.aws_region
 
-  vpc_cidr        = local.vpc_cidr
+  vpc_cidr = local.vpc_cidr
 
-  subnet_cidr     = local.subnet_cidr
+  subnet_cidr = local.subnet_cidr
 
   tags = local.common_tags
 
