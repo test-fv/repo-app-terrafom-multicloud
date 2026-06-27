10f79e7c600e57e69960401254abfe395c3e01c1
Fabian Vargas <fabian_evm@live.com>
2026-06-26 20:05:54 -0500

    fix: restore aws outputs 2

--- terraform/environments/aws/prod/outputs.tf
+++ terraform/environments/aws/prod/outputs.tf
@@ -6,6 +6,10 @@ output "vm_ip" {
   value = module.platform.public_ip
 }
 
+output "instance_id" {
+  value = module.platform.instance_id
+}
+
 output "registry_url" {
   value = module.platform.registry_url
 }
@@ -18,20 +22,11 @@ output "repository_name" {
   value = module.platform.repository_name
 }
 
-output "registry_username" {
-  value = module.platform.registry_username
-}
-
-output "registry_password" {
-  value     = module.platform.registry_password
-  sensitive = true
-}
-
 output "ssh_user" {
   value = module.platform.ssh_user
 }
 
 output "ssh_private_key" {
-  value     = module.platform.ssh_private_key
-  sensitive = true
+  value      = module.platform.ssh_private_key
+  sensitive  = true
 }
--- terraform/stacks/aws/app-platform/outputs.tf
+++ terraform/stacks/aws/app-platform/outputs.tf
@@ -1,20 +1,44 @@
+#############################################
+# Compute
+#############################################
+
 output "public_ip" {
-  description = "Public IP of the virtual machine."
+  description = "Public IP of the EC2 instance."
 
   value = module.compute.public_ip
 }
 
 output "instance_id" {
-  description = "AWS EC2 Instance ID."
+  description = "EC2 Instance ID."
 
   value = module.compute.instance_id
 }
 
+#############################################
+# Registry
+#############################################
+
 output "registry_url" {
-  description = "Container registry URL."
+  description = "Full ECR repository URL."
 
   value = module.registry.registry_url
 }
+
+output "registry_server" {
+  description = "ECR registry hostname."
+
+  value = module.registry.registry_server
+}
+
+output "repository_name" {
+  description = "ECR repository name."
+
+  value = module.registry.repository_name
+}
+
+#############################################
+# Runtime
+#############################################
 
 output "ssh_user" {
   description = "SSH username."
